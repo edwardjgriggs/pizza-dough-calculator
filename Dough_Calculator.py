0543b699a74ad16051ee2862a0e02a9703e8ce64
edwardjgriggs <edwardjgriggs@gmail.com>
2025-10-25 21:20:47 +0000

    Update Dough_Calculator.py

--- Dough_Calculator.py
+++ Dough_Calculator.py
@@ -147,4 +147,4 @@ def index():
     return render_template_string(HTML_TEMPLATE, result=result, error=error)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5080)
